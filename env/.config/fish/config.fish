c0fd5bbca02db3d3d52e84c64666f91183bf2fa2
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2025-01-30 10:33:15 -0300

    small updates

--- env/.config/fish/config.fish
+++ env/.config/fish/config.fish
@@ -4,13 +4,6 @@ end
 
 # Aliases
 alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
-alias copy="tr -d '\n' | pbcopy"
-alias vim="nvim"
-alias commit_message="git diff --cached | cody chat --stdin -m 'Write a commit message for this diff, do not use capital letters and use the angular conventional commits.' | awk '/^```/{if (p) exit; p=1;next} p' | pbcopy"
-
-# tmux
-set tmux (which tmux)
-alias tmux="tmux -2"
 
 # go
 set --export GOPATH "$HOME/go"
@@ -19,13 +12,31 @@ function fish_right_prompt
   # intentionally left blank
 end
 
-export PATH="$PATH:$HOME/.local/bin"
-export XDG_CONFIG_HOME="$HOME/.config"
-export DEV_ENV_HOME="$HOME/personal/dev"
-
 set fish_greeting
 set -g fish_autosuggestion_enabled 0
 
 for color in (set | grep -Eo '^fish_color_[a-zA-Z0-9_]+')
     set $color normal
 end
+
+export XDG_CONFIG_HOME="$HOME/.config"
+set VIM nvim
+
+export PERSONAL="$XDG_CONFIG_HOME/personal/"
+export DEV_ENV="$HOME/dev"
+for i in (find -L $PERSONAL -type f)
+    source $i
+end
+
+export GOPATH="$HOME/.local/go"
+export GIT_EDITOR="$VIM"
+export DEV_ENV_HOME="$HOME/personal/dev"
+
+export PATH="$PATH:$HOME/.local/scripts"
+export PATH="$PATH:$HOME/.local/bin"
+
+export PATH="$PATH:/opt/homebrew/bin"
+export PATH="$PATH:/opt/homebrew/sbin"
+export PATH="$HOME/.local/.npm-global/bin:$PATH"
+export PATH="$HOME/.local/scripts:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
