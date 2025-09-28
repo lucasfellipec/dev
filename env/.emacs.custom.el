c0b1670e9e1d4fd116c99cdf31b46a2f307ba177
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2025-09-28 20:39:59 -0300

    automated dev commit

--- env/.emacs.custom.el
+++ env/.emacs.custom.el
@@ -6,16 +6,16 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
      default))
  '(display-line-numbers-type 'relative)
  '(package-selected-packages
-   (quote
-    (cmake-mode company dash-functional devdocs docker-compose-mode
+   '(cmake-mode company dash-functional devdocs docker-compose-mode
                 dockerfile-mode gnuplot go-mode gruber-darker-theme
-                ido-completing-read+ lua-mode markdown-mode magit
-                multiple-cursors nginx-mode nim-mode paredit rust-mode
-                smex toml-mode typescript-mode yaml-mode yasnippet)))
+                helm-ls-git ido-completing-read+ lua-mode magit
+                markdown-mode move-text multiple-cursors nginx-mode
+                nim-mode paredit rust-mode smex toml-mode
+                typescript-mode yasnippet))
  '(whitespace-style
    '(face tabs spaces trailing space-before-tab newline newline-mark
           indentation empty space-after-tab space-mark tab-mark)))
